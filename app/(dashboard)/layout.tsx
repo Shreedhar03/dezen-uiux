4380879e2c37ad02afbc2e0de846c25a48ec9582
Shreedhar Deodatta Urawane <urawane03@gmail.com>
2024-06-20 11:50:03 +0530

    add Nav component

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -19,6 +19,7 @@ import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
 import { useRouter, usePathname } from "next/navigation";
 import { useTronLink } from "@/hooks/TronHooks";
+import Navbar from "@/components/Navbar";
 
 interface LayoutProps {
   children: ReactNode;
@@ -31,15 +32,7 @@ const Layout = ({ children }: LayoutProps) => {
 
   return (
     <main className="flex h-[80vh] items-center p-3 sm:p-12">
-      <nav className="flex flex-col gap-2 w-2/6">
-        <h1>NAVBAR</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
-          excepturi at obcaecati ex inventore corporis reprehenderit id aliquid
-          aperiam cumque, eaque tempore et. Culpa ab reprehenderit distinctio
-          repellendus eos eaque.
-        </p>
-      </nav>
+      <Navbar />
       <div className="shrink-0">{children}</div>
     </main>
   );
---
+++ components/Navbar.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="flex flex-col gap-2 w-2/6">
+      <h1>NAVBAR</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
+        excepturi at obcaecati ex inventore corporis reprehenderit id aliquid
+        aperiam cumque, eaque tempore et. Culpa ab reprehenderit distinctio
+        repellendus eos eaque.
+      </p>
+    </nav>
+  );
+};
+
+export default Navbar;
