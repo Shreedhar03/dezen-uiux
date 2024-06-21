eb21b3394aee64df2d0a217f878e2913dfb2378b
Shreedhar Deodatta Urawane <urawane03@gmail.com>
2024-06-21 12:55:27 +0530

    explore page

--- app/(dashboard)/explore/page.tsx
+++ app/(dashboard)/explore/page.tsx
@@ -3,6 +3,8 @@ import { useState, useEffect } from "react";
 import { Loader2 } from "lucide-react";
 import { useRouter, usePathname } from "next/navigation";
 import AnimateOnRouteChange from "@/components/AnimateOnRouteChange";
+import localFont from "next/dist/compiled/@next/font/dist/local";
+import local from "next/font/local";
 // import AnimateOnRouteChange from "../components/AnimateOnRouteChange";
 
 interface User {
@@ -25,6 +27,16 @@ interface Memecoin {
   timestamp: Date;
   creator: User;
 }
+
+const kanit = local({
+  src: "../../../public/fonts/kanit.ttf",
+  display: "swap",
+});
+const theme = local({
+  src: "../../../public/fonts/docallismeonstreet.otf",
+  display: "swap",
+});
+
 export default function Home() {
   const [memecoins, setMemecoins] = useState<Memecoin[]>([]);
   const [loading, setLoading] = useState(true);
@@ -51,41 +63,39 @@ export default function Home() {
 
   return (
     <AnimateOnRouteChange>
-      <div className="home-container">
-        {/* Your existing content */}
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis quod
-        adipisci a nulla vero blanditiis temporibus, recusandae sequi rem, iste
-        ipsam iure sit! Blanditiis, ducimus earum assumenda vel eligendi nihil
-        ea molestias quia nemo sed, hic cum beatae, optio maxime ut enim?
-        Similique perferendis ut ducimus dicta quisquam repellendus illo
-        doloremque dolor dolorum. In est error cum nobis quod consectetur,
-        repudiandae vel atque tempora numquam placeat dicta ipsam quas neque
-        nihil laboriosam? Voluptatem, expedita consequatur consectetur beatae
-        voluptate aliquam nostrum commodi officia blanditiis fugit delectus!
-        Temporibus perspiciatis veritatis quos accusantium minus earum
-        praesentium reprehenderit! Assumenda expedita nemo vel eos accusamus
-        perferendis, mollitia et nisi ex temporibus in sunt distinctio
-        recusandae perspiciatis velit eius alias iusto animi, inventore nobis
-        modi saepe? Eius natus fugit aut. Cum, sed corrupti. Nesciunt impedit
-        quae quibusdam omnis incidunt suscipit autem excepturi. Error magni,
-        dignissimos ab quas perferendis eum dolorem, deserunt architecto
-        cupiditate iusto quos. Aliquam ullam laudantium quo enim, beatae
-        similique nobis esse amet. Nam sapiente, corrupti id qui odit, dolore
-        ipsa doloribus eligendi nostrum aliquam nobis iste molestiae nihil
-        fugiat sint ex ducimus pariatur aperiam sit quas nulla mollitia. Eaque
-        eligendi sequi dicta accusamus qui nam repellat fugiat veniam laudantium
-        ea expedita, cupiditate, asperiores magni saepe, error hic praesentium.
-        Harum doloremque aliquam cupiditate commodi soluta. Expedita molestias
-        ducimus dignissimos nesciunt laudantium saepe exercitationem unde aut,
-        facilis suscipit odio, similique ipsa incidunt quisquam vitae illo alias
-        mollitia officiis facere. Asperiores, cumque veniam deleniti voluptates
-        necessitatibus aliquid, voluptatem illo facere animi optio eaque, ipsum
-        sapiente consectetur itaque et delectus earum architecto culpa a commodi
-        ullam! Accusamus eum explicabo quam veritatis possimus quisquam nemo!
-        Animi accusantium minima itaque, veritatis, numquam quo facere molestiae
-        laudantium expedita dolorum eius, recusandae eaque iure sequi sapiente
-        minus. Nostrum adipisci dolor, quasi, vitae laudantium aperiam iusto
-        perspiciatis veniam explicabo odio in dignissimos.
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 my-6 mx-4">
+        {loading ? (
+          <div className="flex justify-center items-center">
+            <Loader2 size={32} />
+          </div>
+        ) : (
+          memecoins.map((memecoin) => (
+            <div
+              key={memecoin.memecoin_address}
+              className={`${kanit.className} bg-secondary p-4 rounded-2xl flex flex-col justify-between gap-3`}
+            >
+              <div>
+                <img
+                  src={`https://ivory-eligible-hamster-305.mypinata.cloud/ipfs/${memecoin.logo}`}
+                  className="h-48 w-full object-cover rounded-2xl"
+                />
+                <h2 className={`${theme.className} text-2xl font-bold mt-2`}>
+                  {memecoin.name}
+                </h2>
+                <p className="mt-2">${memecoin.ticker}</p>
+                <p className="leading-5 mt-2">{memecoin.description}</p>
+              </div>
+              <div className="flex gap-3 items-center">
+                <img
+                  src={`https://ivory-eligible-hamster-305.mypinata.cloud/ipfs/${memecoin.creator.profilePicture}`}
+                  alt={memecoin.name}
+                  className="w-12 h-12 rounded-full"
+                />
+                <p className="text-sm mt-1">{memecoin.creator.username}</p>
+              </div>
+            </div>
+          ))
+        )}
       </div>
     </AnimateOnRouteChange>
   );
