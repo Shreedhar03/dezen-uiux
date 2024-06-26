f0f9be686e996efa799231e9cfa2e812c8e936d8
[AtharvPorate1] <www.atharvporate254@gmail.com>
2024-06-26 22:25:59 +0530

    NavbarDone:

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,6 +7,7 @@ import Image, {StaticImageData} from "next/image";
 import disc from "@/public/dezenDisc.jpg";
 import tiredmnky from "@/public/tiredmnky.png";
 import exploreMnky from "@/public/exploreMnky.jpeg";
+import rocket from '@/public/NavRocket.jpg'
 
 const CurrentPage = ({ hovered, setHovered }) => {
   const pathname = usePathname();
@@ -18,12 +19,7 @@ const CurrentPage = ({ hovered, setHovered }) => {
 
   const optionRef = useRef(null);
   const rotateRef = useRef(null);
-  const discRef1 = useRef(null);
-  const discRef2 = useRef(null);
-  const discRef3 = useRef(null);
-  const discRef4 = useRef(null);
-  const discRef5 = useRef(null);
-  const discRef6 = useRef(null);
+
   const [mouseDown, setMouseDown] = React.useState(0);
 
   useEffect(() => {
@@ -62,81 +58,80 @@ const CurrentPage = ({ hovered, setHovered }) => {
     }
   }, [hovered]);
 
-  useEffect(() => {
-    const rotationDuration = 15;
-    const equalAngle = 360 / 6;
+  // useEffect(() => {
+  //   const rotationDuration = 15;
+  //   const equalAngle = 360 / 6;
 
-    gsap.set(discRef1.current, { rotate: 0 });
-    gsap.set(discRef2.current, { rotate: equalAngle });
-    gsap.set(discRef3.current, { rotate: 2 * equalAngle });
-    gsap.set(discRef4.current, { rotate: 3 * equalAngle });
-    gsap.set(discRef5.current, { rotate: 4 * equalAngle });
-    gsap.set(discRef6.current, { rotate: 5 * equalAngle });
+  //   gsap.set(discRef1.current, { rotate: 0 });
+  //   gsap.set(discRef2.current, { rotate: equalAngle });
+  //   gsap.set(discRef3.current, { rotate: 2 * equalAngle });
+  //   gsap.set(discRef4.current, { rotate: 3 * equalAngle });
+  //   gsap.set(discRef5.current, { rotate: 4 * equalAngle });
+  //   gsap.set(discRef6.current, { rotate: 5 * equalAngle });
 
-    gsap.to([discRef1.current, discRef2.current, discRef3.current, discRef4.current, discRef5.current, discRef6.current], {
-      rotate: "+=720",
-      duration: rotationDuration,
-      ease: "none",
-      repeat: -1,
-      transformOrigin: "left center",
-    });
-  }, []);
+  //   gsap.to([discRef1.current, discRef2.current, discRef3.current, discRef4.current, discRef5.current, discRef6.current], {
+  //     rotate: "+=720",
+  //     duration: rotationDuration,
+  //     ease: "none",
+  //     repeat: -1,
+  //     transformOrigin: "left center",
+  //   });
+  // }, []);
 
   return (
     <div
-      className="fixed hidden lg:block"
+      className="fixed hidden lg:block "
       onMouseEnter={() => setHovered(true)}
       ref={optionRef}
       onMouseLeave={() => setHovered(false)}
     >
-      <Link href="/explore">
-        <div
-          className="fixed left-0 bottom-[40%] w-[20%] h-[10%] bg-zinc-800 transform translate-x-3 border-4  border-black origin-left flex items-center justify-center"
-          ref={discRef1}
-        >
-          <p className="text-3xl font-semibold">explore</p>
-        </div>
-      </Link>
+      
       <Link href="/my-profile">
         <div
-          className="fixed left-0 bottom-[40%] w-[20%] h-[10%] bg-zinc-600 transform translate-x-3 border-4  border-black flex items-center justify-center"
-          ref={discRef2}
+          className="fixed left-0 bottom-[40%] w-[20%] h-[10%]  transform  border-4 -rotate-45 origin-left  border-black flex items-center justify-center overflow-hidden"
+          
         >
-          <p className="text-3xl font-semibold">my-profile</p>
+          <Image
+            src={tiredmnky}
+            alt="tiredmnky"
+            style={{ objectFit: "contain" }}
+            className="fixed"
+          ></Image>
+          <p className="text-3xl font-semibold fixed">my-profile</p>
+          
+ 
         </div>
       </Link>
       <Link href="/launch">
         <div
-          className="fixed left-0 bottom-[40%] w-[20%] h-[10%] bg-zinc-600 transform translate-x-3 border-4 border-black flex items-center justify-center"
-          ref={discRef3}
+          className="fixed left-0 bottom-[40%] w-[20%] h-[10%] bg-zinc-600 transform  border-4 rotate-45 origin-left border-black flex items-center justify-center overflow-hidden"
+          
         >
-          <p className="text-3xl font-semibold">launch</p>
+          <Image
+            src={rocket}
+            alt="tiredmnky"
+            style={{ objectFit: "contain" }}
+            className="fixed"
+          ></Image>
+          <p className="text-3xl font-semibold  fixed">launch</p>
         </div>
       </Link>
       <Link href="/explore">
         <div
-          className="fixed left-0 bottom-[40%] w-[20%] h-[10%] bg-zinc-800 transform translate-x-3 border-4  border-black origin-left flex items-center justify-center"
-          ref={discRef4}
+          className="fixed left-0 bottom-[40%] w-[20%] h-[10%] bg-zinc-800 transform  border-4  border-black origin-left flex items-center justify-center overflow-hidden"
+          
         >
-          <p className="text-3xl font-semibold">explore</p>
+          <Image
+            src={exploreMnky}
+            alt="tiredmnky"
+            style={{ objectFit: "contain" }}
+            className="fixed"
+          ></Image>
+          <p className="text-3xl font-semibold   fixed">explore</p>
+          
         </div>
       </Link>
-      <Link href="/my-profile">
-        <div
-          className="fixed left-0 bottom-[40%] w-[20%] h-[10%] bg-zinc-600 transform translate-x-3 border-4  border-black flex items-center justify-center"
-          ref={discRef5}
-        >
-          <p className="text-3xl font-semibold">my-profile</p>
-        </div>
-      </Link>
-      <Link href="/launch">
-        <div
-          className="fixed left-0 bottom-[40%] w-[20%] h-[10%] bg-zinc-600 transform translate-x-3 border-4 border-black flex items-center justify-center"
-          ref={discRef6}
-        >
-          <p className="text-3xl font-semibold">launch</p>
-        </div>
-      </Link>
+      
     </div>
   );
 };
@@ -147,6 +142,7 @@ const NavSection = () => {
       <div className="fixed left-0 w-[50%] h-[50%] bg-slate-500 rounded-1/2 transform -translate-x-[40%] border-4 border-black"></div>
       <div className="fixed left-0 w-[20%] h-[10%] bg-green-500 transform -translate-x-10 border-4 rotate-[15deg] origin-left border-black">
         <p className="fixed text-3xl font-semibold transform translate-x-[180%] translate-y-2">
+          
           Explore
         </p>
       </div>
@@ -217,6 +213,7 @@ const Navbar = () => {
             alt="tiredmnky"
             style={{ objectFit: "contain" }}
           ></Image>
+
         </div>
       </>
     );
